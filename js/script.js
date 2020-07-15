cf500c46f4411c5639e3bbf279a4c660866e9bf7
FedericoV <vaglicafederico95@gmail.com>
2020-07-15 07:39:58 +0200

    2 version

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 
-for (var i=1; i<=100; i++){
-    if(i%3 == 0 && i%5 == 0) {
-        console.log('fizzbuzz');
-    } else if(i%3 == 0){
-        console.log('fizz')
-    } else if (i%5 == 0){
-        console.log('Buzz')
-    } else {
-        console.log(i);
-    }
+//for (var i=1; i<=100; i++){
+//    if(i%3 == 0 && i%5 == 0) {
+//        console.log('fizzbuzz');
+//    } else if(i%3 == 0){
+//        console.log('fizz')
+//    } else if (i%5 == 0){
+//        console.log('Buzz')
+//    } else {
+//        console.log(i);
+//    }
+//}
+
+
+var fizzBuzz = "";
+
+for (var i=1; i<=100; i++) {
+    var fizzBuzz = "";
+
+    if(i%3 == 0){fizzBuzz += 'Fizz'};
+    if(i%5 == 0){fizzBuzz += 'Buzz'};
+    if(fizzBuzz == "" ){fizzBuzz = i};
+
+    console.log(fizzBuzz)
 }
 
 
